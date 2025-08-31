9ec67617efceeea7a901e1739f0c49667f88a034
sanzida-mim <mim.3ingenious@gmail.com>
2025-09-01 00:00:17 +0600

    Starting API call function.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const BASE_URL = "https://api.fxratesapi.com/latest?base=USD&currencies=BDT&format=json";
+const BASE_URL = "https://api.fxratesapi.com/latest?";
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select").value;
+const toCurr = document.querySelector(".to select").value;
 
 for (let select of dropdowns) {
     for (currCode in countryList) {
@@ -15,4 +18,32 @@ for (let select of dropdowns) {
         }
         select.append(newOption);
     }
+
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
 }
+
+const updateFlag = (el) => {
+    let currCode = el.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+
+    let img = el.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal= amount.value;
+
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    const URL = `${BASE_URL}base=${fromCurr}&currencies=${toCurr}&format=json`;
+
+    
+})
